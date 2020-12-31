454e6d6081b73fa10ef7fdba14ea9795c13b1adb
Harry Carron <hcarron92@gmail.com>
2020-12-31 00:07:44 +0000

    missing moment package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ActivityGraphComponent } from "./components/activity-graph/activity-graph.component";
-
+import { MomentModule } from "ngx-moment";
 @NgModule({
   declarations: [AppComponent, ActivityGraphComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MomentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
