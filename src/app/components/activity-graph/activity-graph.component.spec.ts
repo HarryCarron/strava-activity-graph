2fc4609b51913a0c00959d829166e8bce420f624
Harry Carron <hcarron92@gmail.com>
2021-02-08 13:53:30 +0000

    upgrading to Angular 11 (stage 1)

--- src/app/components/activity-graph/activity-graph.component.spec.ts
+++ src/app/components/activity-graph/activity-graph.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ActivityGraphComponent } from './activity-graph.component';
 
@@ -6,7 +6,7 @@ describe('ActivityGraphComponent', () => {
   let component: ActivityGraphComponent;
   let fixture: ComponentFixture<ActivityGraphComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ActivityGraphComponent ]
     })
