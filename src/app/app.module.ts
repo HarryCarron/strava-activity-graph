b6db60365e9702457a47823672aba328e17538d7
Harry Carron <hcarron92@gmail.com>
2021-02-09 16:20:35 +0000

    Adding FontAwesome correctly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { ActivityGraphComponent } from "./components/activity-graph/activity-graph.component";
 import { DataEditorComponent } from "./components/data-editor/data-editor.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 @NgModule({
   declarations: [AppComponent, ActivityGraphComponent, DataEditorComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
